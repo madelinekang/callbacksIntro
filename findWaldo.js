22c2bab908ffb32f4ecc0bd45654612de0823b49
Madeline Kang <maddyjkang@gmail.com>
2018-08-02 18:09:40 +0000

    refractored to have forEach loop

---
+++ findWaldo.js
@@ -0,0 +1,14 @@
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  arr.forEach(function(name, index) {
+    if (name === "Waldo") {
+      found(index);   // execute callback
+    }
+  });
+}
+
+function actionWhenFound(i) {
+  console.log("Found Waldo at index " + i + " !");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
